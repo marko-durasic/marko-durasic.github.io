1c99c2f75992a4a83dfad86cea4ca44ff042ce38
marko-durasic <marko-durasic@users.noreply.github.com>
2026-04-07 05:59:09 +0000

    deploy: 5cdbf0e05b50fc1baa9bb36dd193f40f78b2f28e

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,31 @@
+var store = [{
+        "title": "Publications",
+        "excerpt":"“An Adaptive Energy-efficient Beaconing Mechanism for Geographic Routing”   you can get the PDF directly.  ","categories": [],
+        "tags": [],
+        "url": "https://www.markodurasic.com/publications/",
+        "teaser":null},{
+        "title": "Terminator",
+        "excerpt":"How to set custom layout and add commands This tutorial is meant for users of Linux and Terminator (terminal emulator). Terminator introduction Terminator is a useful tool for arranging terminals. You can split your terminal horizontally and vertically as much as you heart desires, as well as setup groups and...","categories": [],
+        "tags": [],
+        "url": "https://www.markodurasic.com/terminator/",
+        "teaser":null},{
+        "title": "Golang Interface Example",
+        "excerpt":"Example code Golang (Go) with interface This is an example of how to use interfaces in golang and their benefits File structure . ├── literature # Custom package │ ├── book.go # Custom book struct │ ├── comic.go # Custom comic struct │ └── literature.go # Interface ├── go.mod #...","categories": [],
+        "tags": [],
+        "url": "https://www.markodurasic.com/golang-interface-example/",
+        "teaser":null},{
+        "title": "New Developer Guide",
+        "excerpt":"A Guide for Aspiring Software Developers If you’re just starting out in the world of software development, you may be wondering where to begin. In this blog post, I will provide a suggested learning path for aspiring software developers, including courses and resources for learning the necessary skills and preparing...","categories": [],
+        "tags": [],
+        "url": "https://www.markodurasic.com/new-developer-guide/",
+        "teaser":null},{
+        "title": "S3 Auditor",
+        "excerpt":"AWS S3 Bucket Auditor: Automating Security Checks and Macie Classification ← GitHub Repository, please contribute or provide feedback. The AWS S3 Bucket Auditor is a Go-based command-line tool that performs security audits on your AWS S3 buckets. This tool was built to simplify and automate multiple security checks, including detecting...","categories": [],
+        "tags": [],
+        "url": "https://www.markodurasic.com/s3-auditor/",
+        "teaser":null},{
+        "title": "Cloud Coach - Free Interactive Study Tool for AWS Certifications",
+        "excerpt":"Try it out You can check it out here: Cloud Coach I’ve been working on AWS certifications for a while now, and I kept running into the same problem: most study tools either overwhelm you with information or focus too much on memorizing facts instead of actually understanding concepts. So...","categories": ["AWS","Tools","Study","Free","Certification"],
+        "tags": ["AWS","Study Tool","Interactive","Certification","React","Free","Cloud Coach","Exam Prep","Developer Tools"],
+        "url": "https://www.markodurasic.com/aws/tools/study/free/certification/aws-quick-coach-interactive-study-tool/",
+        "teaser":null}]
